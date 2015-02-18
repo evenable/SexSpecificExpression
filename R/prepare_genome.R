c82164feb1d9e6b9e17be35ba526b14a5df3069d
evenable <Elena_venable@brown.edu>
2015-02-18 15:38:09 -0500

    Edited the prepare_genome function to remove UTRs. Also added the new comparison data that was produced by running the data on the lists without UTRs.

--- R/prepare_genome.R
+++ R/prepare_genome.R
@@ -18,7 +18,9 @@ gene_names_temp <- gsub("^.*A","A",as.character(genome$GeneID))
 genome$GeneID <- gsub("\\..*","",gene_names_temp)
 index <- grep("TE",genome$GeneID)
 genome$GeneID[index] <- gsub("[\\].*","",genome$GeneID[index])
-ID_and_count_temp <- data.frame(GeneID = genome$GeneID, snp_count = (genome$snp_count), gene_length = abs(genome$Start - genome$Stop))
+UTR <- grep(pattern="UTR",genome$Region)
+new_genome <- genome[-UTR,]
+ID_and_count_temp <- data.frame(GeneID = new_genome$GeneID, snp_count = (new_genome$snp_count), gene_length = abs(new_genome$Start - new_genome$Stop))
 ID_and_count <- aggregate(. ~ GeneID, data = ID_and_count_temp, FUN = sum)
 ID_and_count
 }
